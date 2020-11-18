85ac625c62f698f79e79d170cfae1999c81f083f
lexmrco <lexmrco@hotmail.com>
2020-11-18 11:57:57 -0500

    Version inicial Socket io client

---
+++ build/precache-manifest.48a575eaacc6d9a93c13e902b1250197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14238b38a9b8c8ccdbced57b50866b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e6dfc1a0eab29925be",
+    "url": "/static/css/main.fb2de0fa.chunk.css"
+  },
+  {
+    "revision": "2b7517ed4978cfb92689",
+    "url": "/static/js/2.4d1d183d.chunk.js"
+  },
+  {
+    "revision": "4201076bee892946faa480b75f4c1fd4",
+    "url": "/static/js/2.4d1d183d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59e6dfc1a0eab29925be",
+    "url": "/static/js/main.30eacda1.chunk.js"
+  },
+  {
+    "revision": "cdf041b3892fd22a989b",
+    "url": "/static/js/runtime-main.6c427cc5.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, '/build/favicon.ico')));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
